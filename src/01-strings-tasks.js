b8408c2250b4ff8c7063fa50bca5a7a6b224e7b0
0DiNEC <as.odinson@gmail.com>
2023-06-19 22:51:29 +0300

    feat: complete border task

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -202,8 +202,10 @@ function extractEmails(str) {
  *             '└──────────┘\n'
  *
  */
-function getRectangleString(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleString(width, height) {
+  return `┌${'─'.repeat(width - 2)}┐\n${
+    `│${' '.repeat(width - 2)}│\n`.repeat(height - 2)
+  }└${'─'.repeat(width - 2)}┘\n`;
 }
 
 
